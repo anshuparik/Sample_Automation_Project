e25ee1ebf34973fe5939639746d1b86618ac3556
arvind pareek <parik.arvind@gmail.com>
2023-11-04 00:10:06 +0530

    adding for gender check box

--- src/main/java/Forms.java
+++ src/main/java/Forms.java
@@ -1,5 +1,4 @@
 import Objectrepo.ObjofFormspage;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
