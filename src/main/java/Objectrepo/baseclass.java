fe7a6e9ccee00ea83e95d63c70dbfd5c0cfa85fd
arvind pareek <parik.arvind@gmail.com>
2023-11-04 18:08:04 +0530

    changes on framework level

--- src/main/java/Objectrepo/baseclass.java
+++ src/main/java/Objectrepo/baseclass.java
@@ -1,6 +1,32 @@
 package Objectrepo;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class baseclass {
     //invoking driver using single method
+    public void initializeDriver() throws IOException {
+        Properties prop = new Properties();
+        FileInputStream file = new FileInputStream("A:\\Learn Automation\\Sample Automation Project\\src\\main\\java\\Objectrepo\\browserdata.properties");
+        prop.load(file);
+      String browserName =  prop.getProperty("browser");
+
+
+if(browserName=="chrome"){
+    //excute chrome browser
+    System.setProperty("webdriver.chrome.driver","A:\\Learn Automation\\Sample Automation Project\\chromedriver.exe");
+    WebDriver driver = new ChromeDriver();
+
+    } else if (browserName.equals("firefox")) {
+    System.out.println("firefox");
+
+} else if (browserName=="internetexp") {
+    System.out.println("dummy");
+}
 
 }
+
+    }
