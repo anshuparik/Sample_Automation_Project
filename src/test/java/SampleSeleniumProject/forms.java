fa6e8e04680ed2a7a2628603007bcf1bb6035705
arvind pareek <parik.arvind@gmail.com>
2023-11-08 23:20:16 +0530

    adding log4j

--- src/test/java/SampleSeleniumProject/forms.java
+++ src/test/java/SampleSeleniumProject/forms.java
@@ -20,15 +20,20 @@ public class forms extends baseclass {
         driver.get("https://demoqa.com/automation-practice-form");
         ObjofFormspage myelementsofFormspage = new ObjofFormspage(driver);
         myelementsofFormspage.firstname().sendKeys("arvind");
+        log.info("name created");
         myelementsofFormspage.lastname().sendKeys("pareek");
+        log.info("last name created");
         myelementsofFormspage.Email().sendKeys("abc@gmai.com");
+        log.error("email created");
       //myelementsofFormspage.gender().click();
         WebElement mobileno = myelementsofFormspage.mobileno();
         js.executeScript("arguments[0].scrollIntoView();", mobileno);
         myelementsofFormspage.mobileno().sendKeys("1234567890");
+        log.trace("mobile no created");
         WebElement textarea = myelementsofFormspage.currentaddress();
         js.executeScript("arguments[0].scrollIntoView();",textarea);
         myelementsofFormspage.currentaddress().sendKeys("java selenium");
+        log.debug("text area created");
 
     }
 }
