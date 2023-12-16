a193c4b499ebed2c6a807ec28d554378ec10e627
arvind pareek <parik.arvind@gmail.com>
2023-12-16 23:47:30 +0530

    added cases for broken link and url

--- src/test/java/SampleSeleniumProject/elements.java
+++ src/test/java/SampleSeleniumProject/elements.java
@@ -10,14 +10,32 @@ import org.testng.annotations.Test;
 import resources.baseclass;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.util.Iterator;
+import java.net.URL;
 import java.util.List;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 public class elements extends baseclass {
+    public static void verifyLinks(String linkUrl) {
+        try {
+            URL url = new URL(linkUrl);
+
+            //Now we will be creating url connection and getting the response code
+            HttpURLConnection httpURLConnect = (HttpURLConnection) url.openConnection();
+            httpURLConnect.setConnectTimeout(5000);
+            httpURLConnect.connect();
+            if (httpURLConnect.getResponseCode() >= 400) {
+                System.out.println("HTTP STATUS - " + httpURLConnect.getResponseMessage() + "is a broken link");
+            }
+
+            //Fetching and Printing the response code obtained
+            else {
+                System.out.println("HTTP STATUS - " + httpURLConnect.getResponseMessage());
+            }
+        } catch (Exception ignored) {
+        }
+    }
+
     //Used Java inheritance concept here
     @Test
     public void Textbox() throws IOException {
@@ -164,7 +182,7 @@ public class elements extends baseclass {
         {  //need to fix
             List<WebElement> allurl = driver.findElements(By.tagName("p"));
             System.out.println(allurl.size());
-            for (int i =0; i<allurl.size(); i++) {
+            for (int i = 0; i < allurl.size(); i++) {
                 WebElement link = allurl.get(i);
                 String urlname = link.getText();
                 System.out.println(urlname);
@@ -172,7 +190,8 @@ public class elements extends baseclass {
 
             }
 
-}              driver.quit();
+        }
+        driver.quit();
     }
 
     private void verifyLinkStatus(String urlname) {
@@ -205,7 +224,6 @@ public class elements extends baseclass {
     @Test
     public void broken() throws IOException {
         driver = initializeDriver();
-        Objofelementspage myobjofelement = new Objofelementspage(driver);
         driver.get("https://demoqa.com/broken");
 
 
@@ -214,11 +232,10 @@ public class elements extends baseclass {
         System.out.println("Total number of Images on the Page are " + images.size());
 
         //checking the links fetched.
-        for(int index=0;index<images.size();index++)
-        {
-            WebElement image= images.get(index);
-            String imageURL= image.getAttribute("src");
-            System.out.println("URL of Image " + (index+1) + " is: " + imageURL);
+        for (int index = 0; index < images.size(); index++) {
+            WebElement image = images.get(index);
+            String imageURL = image.getAttribute("src");
+            System.out.println("URL of Image " + (index + 1) + " is: " + imageURL);
             verifyLinks(imageURL);
 
             //Validate image display using JavaScript executor
@@ -226,40 +243,16 @@ public class elements extends baseclass {
                 boolean imageDisplayed = (Boolean) ((JavascriptExecutor) driver).executeScript("return (typeof arguments[0].naturalWidth !=\"undefined\" && arguments[0].naturalWidth > 0);", image);
                 if (imageDisplayed) {
                     System.out.println("DISPLAY - OK");
-                }else {
+                } else {
                     System.out.println("DISPLAY - BROKEN");
                 }
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 System.out.println("Error Occured");
             }
         }
 
 
         driver.quit();
-    }
-
-    public static void verifyLinks(String linkUrl)
-    {
-        try
-        {
-            URL url = new URL(linkUrl);
-
-            //Now we will be creating url connection and getting the response code
-            HttpURLConnection httpURLConnect=(HttpURLConnection)url.openConnection();
-            httpURLConnect.setConnectTimeout(5000);
-            httpURLConnect.connect();
-            if(httpURLConnect.getResponseCode()>=400)
-            {
-                System.out.println("HTTP STATUS - " + httpURLConnect.getResponseMessage() + "is a broken link");
-            }
-
-            //Fetching and Printing the response code obtained
-            else{
-                System.out.println("HTTP STATUS - " + httpURLConnect.getResponseMessage());
-            }
-        }catch (Exception ignored) {
-        }
     }
 
 
