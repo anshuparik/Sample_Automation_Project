c052df5f4a400d6ac645b0bc473dba6a53eeaef7
arvind pareek <parik.arvind@gmail.com>
2023-12-16 23:46:09 +0530

    added cases for broken link and url

--- src/test/java/SampleSeleniumProject/elements.java
+++ src/test/java/SampleSeleniumProject/elements.java
@@ -1,6 +1,7 @@
 package SampleSeleniumProject;
 
 import Objectrepo.Objofelementspage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -9,6 +10,12 @@ import org.testng.annotations.Test;
 import resources.baseclass;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Iterator;
+import java.util.List;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class elements extends baseclass {
     //Used Java inheritance concept here
@@ -152,30 +159,109 @@ public class elements extends baseclass {
             // Switch back to the parent window
             driver.switchTo().window(parentWindowHandle);
 
-            driver.quit();
         }
 
-//        {
-//            // Initialize DevTools for the existing driver
-//            DevTools devtools = ((ChromeDriver) driver).getDevTools();
-//            devtools.createSession();
-//            devtools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
-//
-//            // EVENT will be triggered
-//            devtools.addListener(Network.responseReceived(), responseReceived -> {
-//                Response myresponse = responseReceived.getResponse();
-//                System.out.println(myresponse.getStatus());
-        //need to fix
-//            });
+        {  //need to fix
+            List<WebElement> allurl = driver.findElements(By.tagName("p"));
+            System.out.println(allurl.size());
+            for (int i =0; i<allurl.size(); i++) {
+                WebElement link = allurl.get(i);
+                String urlname = link.getText();
+                System.out.println(urlname);
+                verifyLinkStatus(urlname);
 
+            }
 
-        // Click on the link using the existing driver instance
-        // myobjofelement.created().click();
+}              driver.quit();
+    }
 
-        // Add any additional test logic or assertions here
+    private void verifyLinkStatus(String urlname) {
+        try {
+            URL url = new URL(urlname);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
 
+            int statusCode = connection.getResponseCode();
+            System.out.println("Status Code for " + urlname + ": " + statusCode);
+
+            connection.disconnect();
+
+            // You can add additional logic based on the status code if needed
+            if (statusCode == HttpURLConnection.HTTP_OK) {
+                System.out.println("Link is reachable.");
+            } else {
+                System.out.println("Link is not reachable. Check the status code.");
+            }
+
+        } catch (MalformedURLException e) {
+            System.out.println("Invalid URL: " + urlname);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+
+    @Test
+    public void broken() throws IOException {
+        driver = initializeDriver();
+        Objofelementspage myobjofelement = new Objofelementspage(driver);
+        driver.get("https://demoqa.com/broken");
+
+
+        // Storing all elements with img tag in a list of WebElements
+        List<WebElement> images = driver.findElements(By.tagName("img"));
+        System.out.println("Total number of Images on the Page are " + images.size());
+
+        //checking the links fetched.
+        for(int index=0;index<images.size();index++)
+        {
+            WebElement image= images.get(index);
+            String imageURL= image.getAttribute("src");
+            System.out.println("URL of Image " + (index+1) + " is: " + imageURL);
+            verifyLinks(imageURL);
+
+            //Validate image display using JavaScript executor
+            try {
+                boolean imageDisplayed = (Boolean) ((JavascriptExecutor) driver).executeScript("return (typeof arguments[0].naturalWidth !=\"undefined\" && arguments[0].naturalWidth > 0);", image);
+                if (imageDisplayed) {
+                    System.out.println("DISPLAY - OK");
+                }else {
+                    System.out.println("DISPLAY - BROKEN");
+                }
+            }
+            catch (Exception e) {
+                System.out.println("Error Occured");
+            }
+        }
+
+
+        driver.quit();
+    }
+
+    public static void verifyLinks(String linkUrl)
+    {
+        try
+        {
+            URL url = new URL(linkUrl);
+
+            //Now we will be creating url connection and getting the response code
+            HttpURLConnection httpURLConnect=(HttpURLConnection)url.openConnection();
+            httpURLConnect.setConnectTimeout(5000);
+            httpURLConnect.connect();
+            if(httpURLConnect.getResponseCode()>=400)
+            {
+                System.out.println("HTTP STATUS - " + httpURLConnect.getResponseMessage() + "is a broken link");
+            }
+
+            //Fetching and Printing the response code obtained
+            else{
+                System.out.println("HTTP STATUS - " + httpURLConnect.getResponseMessage());
+            }
+        }catch (Exception ignored) {
+        }
+    }
+
 
 }
 
