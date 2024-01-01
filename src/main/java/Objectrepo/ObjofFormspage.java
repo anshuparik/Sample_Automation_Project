d24356b4986762cdbe8f6fdc4eaaad0d09b06e3c
arvind pareek <parik.arvind@gmail.com>
2024-01-02 00:18:14 +0530

    added cases for pratice form

--- src/main/java/Objectrepo/ObjofFormspage.java
+++ src/main/java/Objectrepo/ObjofFormspage.java
@@ -14,39 +14,121 @@ public class ObjofFormspage {
     WebElement lastname;
     @FindBy(xpath = "//input[@id='userEmail']")
     WebElement Email;
-    @FindBy(xpath = "label[@for='gender-radio-1']")
+
+    @FindBy(xpath = "//label[text()='Female']")
     WebElement gender;
+
     @FindBy(xpath = "//input[@id='userNumber']")
     WebElement mobileno;
     @FindBy(xpath = "//textarea[@placeholder='Current Address']")
     WebElement currentaddress;
+
+    @FindBy(id = "dateOfBirthInput")
+    WebElement DOB;
+
+    @FindBy(xpath = "//div[@class='react-datepicker__day react-datepicker__day--017']")
+    WebElement date;
+
+    @FindBy(id = "subjectsInput")
+    WebElement subject;
+
+
+    @FindBy(css = ".custom-checkbox:nth-child(1) > .custom-control-label")
+    WebElement sports;
+
+    @FindBy(id = "uploadPicture")
+    WebElement uploadPicture;
+
+    @FindBy(xpath = "(//div[@class=' css-yk16xz-control'])[1]")
+    WebElement select_state;
+
+    @FindBy(xpath = "//div[text()='Select City']")
+    WebElement Select_City;
+
+    @FindBy(id = "react-select-3-option-3")
+    WebElement rajasthan;
+
+    @FindBy(id = "react-select-4-option-0")
+    WebElement jaipur;
+
+    @FindBy(css = ".custom-checkbox:nth-child(2) > .custom-control-label")
+    WebElement reading;
+
+    @FindBy(id = "submit")
+    WebElement submit;
 
     public ObjofFormspage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
-    public WebElement firstname() {
-        return firstname;
+    public WebElement getSubmit() {
+        return submit;
     }
 
-    public WebElement lastname() {
-        return lastname;
+    public WebElement getSports() {
+        return sports;
     }
 
-    public WebElement Email() {
+    public WebElement getJaipur() {
+        return jaipur;
+    }
+
+    public WebElement getRajasthan() {
+        return rajasthan;
+    }
+
+    public WebElement getSelect_City() {
+        return Select_City;
+    }
+
+    public WebElement getSelect_state() {
+        return select_state;
+    }
+
+    public WebElement getUploadPicture() {
+        return uploadPicture;
+    }
+
+
+    public WebElement getSubject() {
+        return subject;
+    }
+
+
+    public WebElement getReading() {
+        return reading;
+    }
+
+    public WebElement getDate() {
+        return date;
+    }
+
+    public WebElement getDOB() {
+        return DOB;
+    }
+
+    public WebElement getEmail() {
         return Email;
     }
 
-    public WebElement gender() {
+    public WebElement getCurrentaddress() {
+        return currentaddress;
+    }
+
+    public WebElement getFirstname() {
+        return firstname;
+    }
+
+    public WebElement getGender() {
         return gender;
     }
 
-    public WebElement mobileno() {
-        return mobileno;
+    public WebElement getLastname() {
+        return lastname;
     }
 
-    public WebElement currentaddress() {
-        return currentaddress;
+    public WebElement getMobileno() {
+        return mobileno;
     }
 }
--- src/test/java/SampleSeleniumProject/forms.java
+++ src/test/java/SampleSeleniumProject/forms.java
@@ -3,40 +3,121 @@ package SampleSeleniumProject;
 import Objectrepo.ObjofFormspage;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import resources.baseclass;
 
 import java.io.IOException;
+import java.time.Duration;
+
 
 public class forms extends baseclass {
 
     public static Logger log = LogManager.getLogger(forms.class.getName());
 
     @Test
-    public void practice_form() throws IOException {
+    public void practice_form() throws IOException, InterruptedException {
         driver = initializeDriver();
         log.info("driver is initialized");
         JavascriptExecutor js = (JavascriptExecutor) driver;
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.get("https://demoqa.com/automation-practice-form");
+
         log.info("moved to url");
         ObjofFormspage myelementsofFormspage = new ObjofFormspage(driver);
-        myelementsofFormspage.firstname().sendKeys("arvind");
-        log.info("name created");
-        myelementsofFormspage.lastname().sendKeys("pareek");
-        log.info("last name created");
-        myelementsofFormspage.Email().sendKeys("abc@gmai.com");
-        log.error("email created");
-        myelementsofFormspage.gender().click();
-        WebElement mobileno = myelementsofFormspage.mobileno();
-        js.executeScript("arguments[0].scrollIntoView();", mobileno);
-        myelementsofFormspage.mobileno().sendKeys("1234567890");
-        log.trace("mobile no created");
-        WebElement textarea = myelementsofFormspage.currentaddress();
-        js.executeScript("arguments[0].scrollIntoView();", textarea);
-        myelementsofFormspage.currentaddress().sendKeys("java selenium");
-        log.debug("text area created");
+        {
+            myelementsofFormspage.getFirstname().sendKeys("arvind");
+            log.info("name created");
+        }
+        {
+            myelementsofFormspage.getLastname().sendKeys("pareek");
+            log.info("last name created");
+        }
+        {
+            myelementsofFormspage.getEmail().sendKeys("abc@gmai.com");
+            log.error("email created");
+        }
+        {
+            js.executeScript("arguments[0].scrollIntoView();", myelementsofFormspage.getGender());
+            myelementsofFormspage.getGender().click();
+
+        }
+        {
+            WebElement mobileno = myelementsofFormspage.getMobileno();
+            js.executeScript("arguments[0].scrollIntoView();", mobileno);
+            myelementsofFormspage.getMobileno().sendKeys("1234567890");
+            log.trace("mobile no created");
+        }
+
+        {
+            myelementsofFormspage.getDOB().click();
+            myelementsofFormspage.getDate().click();
+        }
+
+        {
+
+            js.executeScript("arguments[0].scrollIntoView();", myelementsofFormspage.getSubject());
+            myelementsofFormspage.getSubject().sendKeys("Com");
+            myelementsofFormspage.getSubject().sendKeys(Keys.ARROW_DOWN);
+            myelementsofFormspage.getSubject().sendKeys(Keys.ENTER);
+            myelementsofFormspage.getSubject().sendKeys("Chem");
+            myelementsofFormspage.getSubject().sendKeys(Keys.ARROW_DOWN);
+            myelementsofFormspage.getSubject().sendKeys(Keys.ENTER);
+
+
+        }
+        {
+            js.executeScript("arguments[0].scrollIntoView();", myelementsofFormspage.getSports());
+            myelementsofFormspage.getSports().click();
+            myelementsofFormspage.getReading().click();
+        }
+        {
+            js.executeScript("arguments[0].scrollIntoView();", myelementsofFormspage.getUploadPicture());
+            String filename = "C:\\Users\\Anshu Pareek\\Downloads\\sampleFile (1).jpeg";
+            myelementsofFormspage.getUploadPicture().sendKeys(filename);
+
+        }
+        {
+            WebElement textarea = myelementsofFormspage.getCurrentaddress();
+            js.executeScript("arguments[0].scrollIntoView();", textarea);
+            myelementsofFormspage.getCurrentaddress().sendKeys("java selenium");
+            log.debug("text area created");
+        }
+
+        {
+            js.executeScript("document.body.style.zoom='50%'");
+            // js.executeScript("arguments[0].scrollIntoView();", myelementsofFormspage.getSelect_state());
+            String state = "NCR";
+            String city = "Delhi";
+          myelementsofFormspage.getSelect_state().click();
+            driver.findElement(By.xpath("//div[@id='stateCity-wrapper']//div[text()='" + state + "']")).click();
+            driver.findElement(By.id("city")).click();
+            driver.findElement(By.xpath("//div[@id='stateCity-wrapper']//div[text()='" + city + "']")).click();
+                   /* myelementsofFormspage.getSelect_state().click();
+                    wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//div[contains(text(),'Select State')]"))));
+                    wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.cssSelector("#state > div"))));
+                    driver.findElement(By.xpath("//div[@id='state']//div[@class='css-1g6gooi']"));
+                    driver.findElement(By.xpath("//body/div[@id='app']/div/div/div/div/div/form[@id='userForm']/div[@id='stateCity-wrapper']/div/div[@id='state']/div/div/div/div[1]"));
+                    myelementsofFormspage.getSelect_state().sendKeys("NCR");
+                    myelementsofFormspage.getSelect_state().sendKeys(Keys.ENTER);
+
+                    driver.findElement(By.xpath("//div[contains(text(),'Select City')]"));
+                    driver.findElement(By.xpath("//body//div[@id='app']//div[@id='city']//div//div//div[2]"));
+                    driver.findElement(By.xpath("//body/div[@id='app']/div/div/div/div/div/form[@id='userForm']/div[@id='stateCity-wrapper']/div/div[@id='city']/div/div/div/div[1]"));
+//                    city.sendKeys("Delhi");
+//                    city.sendKeys(Keys.ENTER);*/
+        }
+        {
+            js.executeScript("arguments[0].scrollIntoView();", myelementsofFormspage.getSubmit());
+            myelementsofFormspage.getSubmit().click();
+        }
+
 
     }
 }
+
+
